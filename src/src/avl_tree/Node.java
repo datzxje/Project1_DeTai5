f202134c0f4e6b0d9d5054b2274a8686c92e6887
datzxje <tudabest12@gmail.com>
2023-08-23 23:02:37 +0700

    Add new avl-tree/Node.java Add new avl-tree/AVLTree.java

---
+++ src/src/avl_tree/Node.java
@@ -0,0 +1,59 @@
+package avl_tree;
+
+import student.Student;
+
+public class Node {
+    protected int studentID;
+    protected Student student;
+    protected int height;
+    protected Node left;
+    protected Node right;
+
+    public Node(int studentID, Student student) {
+        this.studentID = studentID;
+        this.student = student;
+        this.height = 1;
+        this.left = null;
+        this.right = null;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+}
